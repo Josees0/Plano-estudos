05554d5581baaa824334a7807259fed31440193f
José E d S. <112288834+Josees0@users.noreply.github.com>
2025-10-22 10:38:49 -0300

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $db_name = 'clientes';
-    
-    $conn = new mysqli($servername, $username, $password, $db_name, 3306);
-?>
